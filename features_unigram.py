8fc27394a7c05dbdc0e9183bca305b96585912f3
Tyler Wilbers <gavagai@archy.localdomain>
2018-06-18 17:27:14 -0400

    unigram model moved to yelp_tourist_classifier

--- features_unigram.py
+++ features_unigram.py
@@ -1,14 +1,25 @@
 import sklearn
 import pandas as pd
+#from nltk.corpus import stopwords
 
 
-def bag_of_words(reviews):
+def bag_of_words(reviews, **kwargs):
     unigram_vect = sklearn.feature_extraction.text.CountVectorizer(
-                                analyzer ="word",
-                                tokenizer =None,
-                                preprocessor =None,
-                                stop_words =stopwords.words('english'),
-                                max_features= None)
+        analyzer="word",
+        tokenizer=None,
+        preprocessor=None,
+        stop_words=None,
+#        stop_words=stopwords.words('english'),
+        max_features=None)
 
-    vect_fit = unigram_vect.fit_transform(reviews)
+    vect_train = unigram_vect.fit_transform(train)
+    vect_test = unigram_vect.transform(test)
+
+    if kwargs['transform'] == True:
+        unigram_train = unigram_vect.transform(reviews)
+
+        unigram_train = unigram_vect.fit_transform(reviews)
+    elif 
+    unigram_train = unigram_vect.fit_transform(reviews)
+
     return pd.DataFrame(vect_fit.A, columns=unigram_vect.get_feature_names())
